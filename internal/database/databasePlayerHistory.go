80297c7ab68b571128d7750088e7fc9b385b1a8f
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-05-04 15:28:05 +0200

    split database file methods into separate files

---
+++ internal/database/databasePlayerHistory.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"github.com/pinpox/megaclan3000/internal/steamclient"
+)
+
+// GetPlayerHistory returns a PlayerHistory object by fetching the values from
+// the database using a prepared statement.
+func (ds *DataStorage) GetPlayerHistory(steamID string) (steamclient.PlayerHistory, error) {
+	ph := steamclient.PlayerHistory{}
+	var err error
+
+	if rows, err := ds.statements["select_player_history"].Query(steamID); err == nil {
+		rows.Scan(
+			&ph.SteamID,
+			&ph.Time,
+			&ph.TotalKills,
+		)
+	}
+	return ph, err
+}
---
+++ internal/database/databasePlayerSummary.go
@@ -0,0 +1,64 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/pinpox/megaclan3000/internal/steamclient"
+)
+
+// GetPlayerSummary returns a PlayerSummary object by fetching the values from
+// the database using a prepared statement.
+func (ds *DataStorage) GetPlayerSummary(steamID string) (steamclient.PlayerSummary, error) {
+
+	ps := steamclient.PlayerSummary{}
+	var err error
+
+	if rows, err := ds.statements["select_player_summary"].Query(steamID); err == nil {
+		for rows.Next() {
+			rows.Scan(
+				&ps.SteamID,
+				&ps.Communityvisibilitystate,
+				&ps.Profilestate,
+				&ps.Personaname,
+				&ps.Profileurl,
+				&ps.Avatar,
+				&ps.Avatarmedium,
+				&ps.Avatarfull,
+				&ps.Lastlogoff,
+				&ps.Personastate,
+				&ps.Primaryclanid,
+				&ps.Timecreated,
+			)
+		}
+	}
+	return ps, err
+}
+
+func (ds *DataStorage) UpdatePlayerSummary(ps steamclient.PlayerSummary) error {
+
+	var result sql.Result
+	var err error
+
+	if result, err = ds.statements["update_player_summary"].Exec(
+		ps.SteamID,
+		ps.Communityvisibilitystate,
+		ps.Profilestate,
+		ps.Personaname,
+		ps.Profileurl,
+		ps.Avatar,
+		ps.Avatarmedium,
+		ps.Avatarfull,
+		ps.Lastlogoff,
+		ps.Personastate,
+		ps.Primaryclanid,
+		ps.Timecreated,
+	); err != nil {
+		return err
+	}
+
+	rows, err := result.RowsAffected()
+	log.Println("Rows affected:", rows)
+	log.Println("Added", ps.SteamID, ps.Personaname, "to player_summary table")
+	return err
+}
