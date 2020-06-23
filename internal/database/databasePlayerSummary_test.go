e154614b9ef49acf19c31888b8cf45f5ea123cf2
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-06-24 01:05:00 +0200

    Make tests fail without proper GetPlayerInfo test
    - add test stub
    - remove old test

--- internal/database/databasePlayerSummary_test.go
+++ internal/database/databasePlayerSummary_test.go
@@ -1,91 +1,38 @@
 package database
 
-// import (
-// 	"testing"
+import (
+	"database/sql"
+	"testing"
 
-// 	"github.com/google/go-cmp/cmp"
-// 	"github.com/pinpox/megaclan3000/internal/steamclient"
-// )
+	"github.com/jmoiron/sqlx"
+	"github.com/pinpox/megaclan3000/internal/steamclient"
+)
 
-// TODO Fix this test or move to sqlx
-// func TestDataStorage_UpdatePlayerSummary(t *testing.T) {
-// 	tests := []struct {
-// 		name    string
-// 		ps      steamclient.PlayerSummary
-// 		wantErr bool
-// 	}{
-// 		{
-// 			name: "Update all colums for existing ID (ID: all_columns)",
-// 			ps: steamclient.PlayerSummary{
-// 				SteamID:                  "all_columns",
-// 				Avatar:                   "0",
-// 				Avatarfull:               "1",
-// 				Avatarmedium:             "2",
-// 				Cityid:                   "3",
-// 				Commentpermission:        "4",
-// 				Communityvisibilitystate: "5",
-// 				Gameextrainfo:            "6",
-// 				Gameid:                   "7",
-// 				Gameserverip:             "8",
-// 				Lastlogoff:               "9",
-// 				Loccityid:                "10",
-// 				Loccountrycode:           "11",
-// 				Locstatecode:             "12",
-// 				Personaname:              "13",
-// 				Personastate:             "14",
-// 				Primaryclanid:            "15",
-// 				Profilestate:             "16",
-// 				Profileurl:               "17",
-// 				Realname:                 "18",
-// 				Timecreated:              "19",
-// 			},
-// 			wantErr: false,
-// 		},
-// 		{
-// 			name: "Update all colums for new ID (ID: all_new)",
-// 			ps: steamclient.PlayerSummary{
-// 				SteamID:                  "all_new",
-// 				Avatar:                   "new0",
-// 				Avatarfull:               "new1",
-// 				Avatarmedium:             "new2",
-// 				Cityid:                   "new3",
-// 				Commentpermission:        "new4",
-// 				Communityvisibilitystate: "new5",
-// 				Gameextrainfo:            "new6",
-// 				Gameid:                   "new7",
-// 				Gameserverip:             "new8",
-// 				Lastlogoff:               "new9",
-// 				Loccityid:                "new10",
-// 				Loccountrycode:           "new11",
-// 				Locstatecode:             "new12",
-// 				Personaname:              "new13",
-// 				Personastate:             "new14",
-// 				Primaryclanid:            "new15",
-// 				Profilestate:             "new16",
-// 				Profileurl:               "new17",
-// 				Realname:                 "new18",
-// 				Timecreated:              "new19",
-// 			},
-// 			wantErr: false,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			prepareDB()
-
-// 			if err := db.UpdatePlayerSummary(tt.ps); (err != nil) != tt.wantErr {
-// 				t.Errorf("DataStorage.UpdatePlayerSummary() error = %v, wantErr %v", err, tt.wantErr)
-// 			}
-
-// 			if got, err := db.GetPlayerSummary(tt.ps.SteamID); err == nil {
-// 				if diff := cmp.Diff(tt.ps, got); diff != "" {
-// 					t.Errorf("DataStorage.UpdatePlayerSummary() mismatch (-want +got):\n%s", diff)
-// 				}
-// 			} else {
-// 				if tt.wantErr == (err != nil) {
-// 					t.Errorf("DataStorage.UpdatePlayerSummary() error = %v, wantErr %v", err, tt.wantErr)
-// 				}
-// 			}
-// 		})
-// 	}
-// }
+func TestDataStorage_UpdatePlayerSummary(t *testing.T) {
+	type fields struct {
+		db         *sqlx.DB
+		statements map[string]*sql.Stmt
+	}
+	type args struct {
+		ps steamclient.PlayerSummary
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ds := &DataStorage{
+				db:         tt.fields.db,
+				statements: tt.fields.statements,
+			}
+			if err := ds.UpdatePlayerSummary(tt.args.ps); (err != nil) != tt.wantErr {
+				t.Errorf("DataStorage.UpdatePlayerSummary() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 
 	"github.com/go-testfixtures/testfixtures/v3"
-	// "github.com/google/go-cmp/cmp"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/pinpox/megaclan3000/internal/steamclient"
 )
 
@@ -36,75 +34,6 @@ func prepareDB() {
 		panic(err)
 	}
 }
-
-//TODO fix or remove test
-// func TestDataStorage_GetPlayerInfoBySteamID(t *testing.T) {
-
-// 	// Needed here because other tests modify the database
-// 	prepareDB()
-
-// 	// The methods called here are tested on their own, so we might aswell use
-// 	// the output instead of typing all fields again by hand
-// 	summary, err := db.GetPlayerSummary("all_columns")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	stats, err := db.GetUserStatsForGame("all_columns")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	recent, err := db.GetRecentlyPlayedGames("all_columns")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	history, err := db.GetPlayerHistory("all_columns")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	tests := []struct {
-// 		name    string
-// 		steamID string
-// 		want    steamclient.PlayerInfo
-// 		wantErr bool
-// 	}{
-// 		{
-// 			name:    "Retrieve PlayerInfo from fixtures (ID: all_columns)",
-// 			steamID: "all_columns",
-// 			want: steamclient.PlayerInfo{
-// 				PlayerSummary:       summary,
-// 				UserStatsForGame:    stats,
-// 				RecentlyPlayedGames: recent,
-// 				PlayerHistory:       history,
-// 			},
-// 			wantErr: false,
-// 		},
-// 		{
-// 			name:    "Retrieve PlayerInfo from fixtures (ID: no_exist)",
-// 			steamID: "no_exist",
-// 			want: steamclient.PlayerInfo{
-// 				PlayerSummary:       steamclient.PlayerSummary{},
-// 				UserStatsForGame:    steamclient.UserStatsForGame{},
-// 				RecentlyPlayedGames: steamclient.RecentlyPlayedGames{},
-// 				PlayerHistory:       steamclient.PlayerHistory{},
-// 			},
-// 			wantErr: true,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			prepareDB()
-// 			got, err := db.GetPlayerInfoBySteamID(tt.steamID)
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("DataStorage.GetPlayerInfoBySteamID() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
-// 			if diff := cmp.Diff(tt.want, got); diff != "" {
-// 				t.Errorf("DataStorage.GetPlayerInfoBySteamID() mismatch (-want +got):\n%s", diff)
-// 			}
-// 		})
-// 	}
-// }
 
 func TestDataStorage_GetAllPlayers(t *testing.T) {
 	tests := []struct {
@@ -157,6 +86,38 @@ func TestDataStorage_UpdatePlayerInfo(t *testing.T) {
 			prepareDB()
 			if err := db.UpdatePlayerInfo(tt.pi); (err != nil) != tt.wantErr {
 				t.Errorf("DataStorage.UpdatePlayerInfo() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+// TODO WRITE THIS TEST, IMPORTANT!
+func TestDataStorage_GetPlayerInfoBySteamID(t *testing.T) {
+	tests := []struct {
+		name    string
+		steamID string
+		want    steamclient.PlayerInfo
+		wantErr bool
+	}{
+		{
+			name:    "Retrieve complete PlayerInfo for all_columns",
+			steamID: "all_columns",
+			want:    steamclient.PlayerInfo{
+				//TODO
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			prepareDB()
+			got, err := db.GetPlayerInfoBySteamID(tt.steamID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DataStorage.GetPlayerInfoBySteamID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DataStorage.GetPlayerInfoBySteamID() = %v, want %v", got, tt.want)
 			}
 		})
 	}
