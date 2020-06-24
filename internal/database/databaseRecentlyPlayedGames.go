cbc14f04c5b11519062e529a5379280fe9fe0801
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-06-24 23:56:48 +0200

    use modl for UpdateRecentlyPlayedGames

--- internal/database/databaseRecentlyPlayedGames.go
+++ internal/database/databaseRecentlyPlayedGames.go
@@ -1,37 +1,27 @@
 package database
 
 import (
+	"github.com/jmoiron/modl"
 	"github.com/pinpox/megaclan3000/internal/steamclient"
+	log "github.com/sirupsen/logrus"
 )
 
 // UpdateRecentlyPlayedGames receives a RecentlyPlayedGames from a PlayerInfo
 // object and updates the databaes for this steamID
 func (ds *DataStorage) UpdateRecentlyPlayedGames(rpg steamclient.RecentlyPlayedGames) error {
 
-	_, err := ds.db.NamedExec(
-		`INSERT OR REPLACE INTO recently_played (
-			steamid,
-            appid,
-            img_icon_url,
-            img_logo_url,
-            name,
-            playtime_2_weeks,
-            playtime_forever,
-            playtime_linux_forever,
-            playtime_mac_forever,
-            playtime_windows_forever)
-		VALUES (
-			:steamid,
-            :appid,
-            :img_icon_url,
-            :img_logo_url,
-            :name,
-            :playtime_2_weeks,
-            :playtime_forever,
-            :playtime_linux_forever,
-            :playtime_mac_forever,
-            :playtime_windows_forever)
-	`, &rpg)
+	dbm := modl.NewDbMap(ds.db.DB, modl.SqliteDialect{})
+	dbm.AddTableWithName(steamclient.RecentlyPlayedGames{}, "recently_played").SetKeys(false, "steamid")
 
-	return err
+	if err := dbm.CreateTablesIfNotExists(); err != nil {
+		log.Warn("Database not creatable: ", err)
+		return err
+	}
+
+	if _, err := dbm.Delete(&rpg); err != nil {
+		return err
+	}
+
+	return dbm.Insert(&rpg)
+
 }
