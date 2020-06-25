e3528e3dc55e1a1e984d5a4af45fad9d460f3335
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-06-25 11:48:12 +0200

    Move single dbMap to struct instead of methods

--- internal/database/databaseUserStatsForGame.go
+++ internal/database/databaseUserStatsForGame.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"github.com/jmoiron/modl"
-
 	"github.com/sirupsen/logrus"
 
 	"github.com/pinpox/megaclan3000/internal/steamclient"
@@ -12,26 +10,20 @@ import (
 // corresponding entry in the database for the steamID
 func (ds *DataStorage) UpdateUserStatsForGame(stats steamclient.UserStatsForGame) error {
 
-	dbm := modl.NewDbMap(ds.db.DB, modl.SqliteDialect{})
-	dbm.AddTableWithName(steamclient.GameStats{}, "player_stats").SetKeys(false, "steamid")
-	dbm.AddTableWithName(steamclient.GameExtras{}, "player_extra").SetKeys(false, "steamid")
-
-	if err := dbm.CreateTablesIfNotExists(); err != nil {
-		logrus.Fatal("Database not creatable: ", err)
+	if _, err := ds.dbm.Delete(&stats.Stats); err != nil {
 		return err
 	}
 
-	if _, err := dbm.Delete(&stats.Stats); err != nil {
+	if _, err := ds.dbm.Delete(&stats.Extra); err != nil {
 		return err
 	}
 
-	if _, err := dbm.Delete(&stats.Extra); err != nil {
-		return err
-	}
-	if err := dbm.Insert(&stats.Stats); err != nil {
+	logrus.Debugf("Inserting id %v into player_stats", stats.Stats.SteamID)
+	if err := ds.dbm.Insert(&stats.Stats); err != nil {
 		return err
 	}
 
-	return dbm.Insert(&stats.Extra)
+	logrus.Debugf("Inserting id %v into player_extra", stats.Extra.SteamID)
+	return ds.dbm.Insert(&stats.Extra)
 
 }
