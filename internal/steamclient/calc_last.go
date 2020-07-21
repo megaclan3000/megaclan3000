d7b51017786f6974a259a1ab704ade5aefd27d58
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-07-21 10:18:36 +0200

    fix doc comment formatting

--- internal/steamclient/calc_last.go
+++ internal/steamclient/calc_last.go
@@ -28,7 +28,7 @@ func (pi PlayerInfo) LastMatch() LastMatch {
 	}
 }
 
-// Lastmatch hold additional stats calculated to be viewed in the templates
+// LastMatch hold additional stats calculated to be viewed in the templates
 type LastMatch struct {
 	Outcome           string
 	FavWeaponIconPath string
