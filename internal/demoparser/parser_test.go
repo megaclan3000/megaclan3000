bd32379bb8c5861c91face38d3bcfaa5fb0aa052
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-07-25 17:07:10 +0200

    add more test values

--- internal/demoparser/parser_test.go
+++ internal/demoparser/parser_test.go
@@ -1,8 +1,9 @@
 package demoparser
 
 import (
-	"reflect"
+	"github.com/google/go-cmp/cmp"
 	"testing"
+	"time"
 )
 
 func TestMyParser_Parse(t *testing.T) {
@@ -16,7 +17,257 @@ func TestMyParser_Parse(t *testing.T) {
 			name: "Parse demo1 file",
 			path: "testdata/demo1.dem",
 			want: Match{
-				Map: "de_mirage",
+				// TODO find out how to get a proper ID
+				ID: "Test",
+
+				// TODO find out how to get the time when the match was played
+				Time: time.Date(2020, time.July, 1, 3, 4, 5, 6, time.UTC),
+				Map:  "de_mirage",
+
+				// Match in the demo took 22 rounds
+				// TODO fill in correct values
+				Rounds: []Round{
+					{
+						Number:       1,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+					{
+						Number:       2,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       3,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       4,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       5,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       6,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       7,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       8,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       9,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       10,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       11,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       12,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       13,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       14,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       15,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       16,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       17,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       18,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       19,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       20,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       21,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+
+					{
+						Number:       22,
+						TickStart:    1,
+						TickEnd:      1,
+						TeamWon:      "CT",
+						BombPlanted:  true,
+						BombDefused:  true,
+						BombExploded: true,
+						Players:      []Player{},
+					},
+				},
 			},
 			wantErr: false,
 		},
@@ -30,8 +281,8 @@ func TestMyParser_Parse(t *testing.T) {
 				t.Errorf("MyParser.Parse() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("MyParser.Parse() = %v, want %v", got, tt.want)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("MyParser.Parse() mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
