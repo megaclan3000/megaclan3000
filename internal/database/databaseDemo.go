2fe4b66e84dbaa46024ad8c98a950c9e6518451a
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-07-27 21:49:27 +0200

    delete falsely restored files

--- internal/database/databaseDemo.go
+++
@@ -1,9 +0,0 @@
-package database
-
-import (
-	"github.com/megaclan3000/megaclan3000/internal/demoparser"
-)
-
-func (ds *DataStorage) InsertDemoScores(scores []demoparser.Score) error {
-	return ds.dbm.Insert(&scores)
-}
--- internal/database/databaseScoreboard.go
+++
@@ -1,7 +0,0 @@
-package database
-
-import "github.com/megaclan3000/megaclan3000/internal/demoparser"
-
-type ScoreBoard struct {
-	PlayerScores []demoparser.Score
-}
