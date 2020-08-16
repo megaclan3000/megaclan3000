00c1fcd424ff50c3ab3307e200b589254133fb9a
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-08-16 11:01:44 +0200

    add snakey chart with api data

--- api.go
+++ api.go
@@ -16,6 +16,37 @@ func apiHandler(vars map[string]string) []byte {
 	case "playerinfo":
 		strconv.ParseUint(vars["id"], 10, 64)
 		byt = []byte(`Here is info about player: ` + vars["steamid"])
+	case "test3":
+		byt = []byte(`
+		[
+
+	["PlayerA1", "PlayerB1", 5],
+	["PlayerA1", "PlayerB2", 5],
+	["PlayerA1", "PlayerB3", 5],
+	["PlayerA1", "PlayerB4", 5],
+	["PlayerA1", "PlayerB5", 5],
+	["PlayerA2", "PlayerB1", 5],
+	["PlayerA2", "PlayerB2", 5],
+	["PlayerA2", "PlayerB3", 5],
+	["PlayerA2", "PlayerB4", 5],
+	["PlayerA2", "PlayerB5", 5],
+	["PlayerA3", "PlayerB1", 5],
+	["PlayerA3", "PlayerB2", 5],
+	["PlayerA3", "PlayerB3", 5],
+	["PlayerA3", "PlayerB4", 5],
+	["PlayerA3", "PlayerB5", 5],
+	["PlayerA4", "PlayerB1", 5],
+	["PlayerA4", "PlayerB2", 5],
+	["PlayerA4", "PlayerB3", 5],
+	["PlayerA4", "PlayerB4", 5],
+	["PlayerA4", "PlayerB5", 5],
+	["PlayerA5", "PlayerB1", 5],
+	["PlayerA5", "PlayerB2", 5],
+	["PlayerA5", "PlayerB3", 5],
+	["PlayerA5", "PlayerB4", 5],
+	["PlayerA5", "PlayerB5", 5]
+		]`)
+
 	case "test":
 		byt = []byte(`
 [
