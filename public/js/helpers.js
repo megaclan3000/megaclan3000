1d8228d54904d2612cc893fd6067139bcf083736
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-08-17 19:19:15 +0200

    add tablejson helper

---
+++ public/js/helpers.js
@@ -0,0 +1,39 @@
+function CreateTableFromJSON(tableid, jsonurl) {
+
+	var table = document.getElementById(tableid);
+		var tbody = document.createElement("tbody");
+
+		$.getJSON(url,  function( data) {
+		for (var row = 0; row < data.length; row++) {
+
+			var tr = document.createElement("tr");
+
+			Object.keys(data[row]).forEach(function(col) {
+				var td = document.createElement("td");
+				td.innerHTML = data[row][col];
+				tr.appendChild(td);
+
+			})
+			tbody.appendChild(tr);
+		}
+	});
+	table.appendChild(tbody);
+}
+
+// Create table, optionally with head
+//
+// <table id="mytable">
+// 	<thead>
+// 		<tr>
+// 			<th>Name</th>
+// 			<th>Item Name</th>
+// 			<th>Item Price</th>
+// 			<th>Item Price</th>
+// 		</tr>
+// 	</thead>
+// </table>
+//
+// Pass url and tableid
+//
+// url = "http://localhost:9090/test.json"
+// CreateTableFromJSON("mytable", url);
