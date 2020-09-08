7041481ec7c314f7bdc060b8f551ffb3faa1efbe
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-09-08 09:26:14 +0200

    implement api_claninfo and landing page

--- api_claninfo.go
+++ api_claninfo.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func handlerAPIClaninfo(w http.ResponseWriter, r *http.Request) {
 
 	var byt []byte
+	var err error
 
-	//TODO query database for vars := mux.Vars(r)
+	vars := mux.Vars(r)
+
+	switch vars["endpoint"] {
+	case "matches":
+		if byt, err = json.Marshal(datastorage.GetMatches()); err != nil {
+			panic(err)
+		}
+	case "players":
+		if byt, err = json.Marshal(datastorage.GetPlayers()); err != nil {
+			panic(err)
+		}
+	case "updates":
+		if byt, err = json.Marshal(datastorage.GetUpdates()); err != nil {
+			panic(err)
+		}
+	default:
+		if byt, err = json.Marshal(
+			struct {
+				Matches interface{} `json:"matches"`
+				Players interface{} `json:"players"`
+				Updates interface{} `json:"updates"`
+			}{
+				Matches: datastorage.GetMatches(),
+				Players: datastorage.GetPlayers(),
+				Updates: datastorage.GetUpdates(),
+			}); err != nil {
+			panic(err)
+		}
+	}
 	// byt, err = json.Marshal(datastorage.GetMatches())
 
 	w.Header().Set("Content-Type", "application/json")
--- datastorage.go
+++ datastorage.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"strconv"
 	"time"
 
 	// "github.com/megaclan3000/megaclan3000/internal/demoparser"
@@ -59,4 +60,148 @@ func (ds DataStorage) GetPlayerInfoBySteamID(steamID uint64) (steamclient.Player
 		}
 	}
 	return steamclient.PlayerInfo{}, errors.New("Player not found")
+}
+
+func (ds DataStorage) GetMatches() interface{} {
+	//TODO implement real data
+	ret := []struct {
+		MapName    string    `json:"map"`         // Name of the map
+		ScoreClan  int       `json:"score_clan"`  // Points clan
+		ScoreEnemy int       `json:"score_enemy"` // Points enemy
+		Time       time.Time `json:"time"`        // Time it was played/uploaded
+		Result     int       `json:"result"`      // Resunt: 1=won, 0=draw, -1=lost
+		MatchID    string    `json:"matchid"`     // ID of the match, for links
+	}{
+		{
+			MapName:    "de_dust2",
+			ScoreClan:  16,
+			ScoreEnemy: 4,
+			Time:       time.Now(),
+			Result:     1,
+			MatchID:    "1",
+		},
+		{
+			MapName:    "de_mirage",
+			ScoreClan:  16,
+			ScoreEnemy: 5,
+			Time:       time.Now(),
+			Result:     1,
+			MatchID:    "1",
+		},
+		{
+			MapName:    "de_inferno",
+			ScoreClan:  5,
+			ScoreEnemy: 16,
+			Time:       time.Now(),
+			Result:     -1,
+			MatchID:    "1",
+		},
+		{
+			MapName:    "de_cache",
+			ScoreClan:  15,
+			ScoreEnemy: 15,
+			Time:       time.Now(),
+			Result:     0,
+			MatchID:    "1",
+		},
+	}
+
+	return ret
+}
+
+func (ds DataStorage) GetPlayers() interface{} {
+
+	type Player struct {
+		PlayerName string `json:"player_name"`
+		SteamID64  string `json:"steamid"`
+		Avatar     string `json:"avatar"`
+		Matches    int    `json:"matches"`
+
+		Kills  int `json:"kills"`
+		Deaths int `json:"deaths"`
+		Hits   int `json:"hits"`
+		Shots  int `json:"shots"`
+
+		Hours  string `json:"hours"`
+		Wins   int    `json:"wins"`
+		Points int    `json:"points"`
+		Status string `json:"status"`
+	}
+
+	var ret []Player
+
+	for _, v := range ds.Players {
+		ret = append(ret, Player{
+			PlayerName: v.PlayerSummary.Personaname,
+			SteamID64:  strconv.FormatUint(v.UserStatsForGame.SteamID, 10),
+			Avatar:     v.PlayerSummary.Avatarfull,
+			Matches:    0, //TODO
+
+			Kills:  v.UserStatsForGame.Stats.TotalKills,
+			Deaths: v.UserStatsForGame.Stats.TotalDeaths,
+			Hits:   v.UserStatsForGame.Stats.TotalShotsHit,
+			Shots:  v.UserStatsForGame.Stats.TotalShotsFired,
+
+			Hours:  v.RecentlyPlayedGames.PlaytimeForever,
+			Wins:   v.UserStatsForGame.Stats.TotalMatchesWon,
+			Points: 0, //TODO
+			Status: v.PlayerSummary.Personastate,
+		})
+	}
+	return ret
+}
+
+func (ds DataStorage) GetUpdates() interface{} {
+
+	type UpdateType int
+
+	const (
+		Award     UpdateType = 0
+		Rank                 = 1
+		MatchWon             = 2
+		MatchLost            = 3
+		MatchDraw            = 4
+	)
+
+	type Update struct {
+		Time  time.Time
+		Type  UpdateType
+		Text1 string
+		Text2 string
+	}
+
+	ret := []Update{
+		{
+			Type:  0,
+			Time:  time.Now(),
+			Text1: "randolf received award",
+			Text2: "Warmup-Killer",
+		},
+		{
+			Type:  1,
+			Time:  time.Now(),
+			Text1: "salatkopf ranked up",
+			Text2: "4 to 5",
+		},
+		{
+			Type:  2,
+			Time:  time.Now(),
+			Text1: "Clan won match",
+			Text2: "de_dust2",
+		},
+		{
+			Type:  3,
+			Time:  time.Now(),
+			Text1: "Clan lost match",
+			Text2: "de_mirage",
+		},
+		{
+			Type:  4,
+			Time:  time.Now(),
+			Text1: "Clan draw match",
+			Text2: "de_inferno",
+		},
+	}
+
+	return ret
 }
